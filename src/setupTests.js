012f65eb211b895d145b650db053e7957ebaa3e2
JyotiNetwork <jyotik2308@gmail.com>
2025-02-13 23:39:39 +0530

    []

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,24 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { render } from '@testing-library/react';
+
+// 👇 Lazy import to avoid issues with Jest
+const AllTheProviders = ({ children }) => {
+  const { createTheme, ThemeProvider } = require('@mui/material/styles'); // Use CommonJS here
+  const { CssBaseline } = require('@mui/material');
+
+  const theme = createTheme();
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+};
+
+const customRender = (ui, options) =>
+  render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from '@testing-library/react';
+export { customRender as render };
